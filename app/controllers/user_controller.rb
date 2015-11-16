ebd770db444b0a7ed6a536de5d2a7e9381b6df25
Taja Cox <tajac90@gmail.com>
2015-11-16 17:37:35 -0500

    adding Users

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,18 @@
+class UserController < ApplicationController
+	def new
+	end
+		def create
+	@user = User.new(user_params)
+		if @user.save
+			flash[:notice] = "Welcome to the site!"
+			redirect_to "/"
+		else
+			flash[:alert] = "There was a problem creating your account. Please try again."
+			redirect_to :back
+		end
+	private
+		def user_params
+			params.require(:user).permit(:username, :password, :password_confirmation)
+		end
+	end
+end 
