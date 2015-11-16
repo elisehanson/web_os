7165cfecca55a272bd700b56fc6cf035d90585df
Elise Hanson <elisehansonf@gmail.com>
2015-11-16 17:46:23 -0500

    application layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :users
-  get'/'=> 'home#index'
-  get "/log-in" => "sessions#new"
-  post "/log-in" => "sessions#create"
-  get "/log-out" => "sessions#destroy", as: :log_out
+
+    get'/'=> 'home#index'
+    get "/log-in" => "sessions#new"
+    post "/log-in" => "sessions#create"
+    get "/log-out" => "sessions#destroy", as: :log_out
+    
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
