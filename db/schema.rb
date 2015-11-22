8101704ef519d0abad725c27b02b250d735daa94
Victor Tsay <vt0826@hotmail.com>
2015-11-22 03:12:10 -0500

    adding game app

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120211401) do
+ActiveRecord::Schema.define(version: 20151121080514) do
 
   create_table "photos", force: :cascade do |t|
     t.datetime "created_at",           null: false
